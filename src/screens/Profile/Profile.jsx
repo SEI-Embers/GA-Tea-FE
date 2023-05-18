53bfc47489f4684cc086e9f05be523ea7e3a2774
CrissCross56 <grant.terdoslavich@gmail.com>
2023-05-18 18:04:19 -0400

    the modal now works, working to get

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -2,110 +2,201 @@ import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import ReactModal from 'react-modal'
 import { useState, useEffect } from 'react'
+// import { setSelectionRange } from '@testing-library/user-event/dist/utils'
+import { Container } from 'postcss'
 export default function Profile(props) {
-  const [pop, setPop] = useState(false)
-  const [testBio, setBio] = useState('')
-  const [testGoals, setGoals] = useState('')
-  const [testSkills, setSkills] = useState([])
-  const [testUsername, setTestUsername] = ''
 
-   const[testProfile, setTestProfile] = useState({
-    username: '',
-    bio: '',
-    skills:[''],
-  })
-  // const [intermediateSkills, setInterMediateSkills] = useState([]);
-  
+
+
+  const [showModal, setShowModal] = useState(false)
+
   //replace this code below V with the code for setting up the profile with props
   const[johnProfile, setJohn] = useState({
     username:"The World's Fault",
     bio: 'eats birds',
     skills: ['eating asphalt','making kids cry'],
+    github: '',
+    linkedin: '',
+    pic: '',
+    email: ''
 
   })
+  //set the inital value of the test value to the value of the object passed to the profile
+  const[testProfile, setTestProfile] = useState(johnProfile)
 
-  function popUp(){
-    setPop(!pop)
-  }
+  // useEffect(() => {
+  //   console.log(testProfile);
+  // }, [testProfile]);
 
-  function updateSkills(e){
-    let length = johnProfile.skills.length()
-    let newArr = []
-    for(let i = 0; i < length; i ++){
-      newArr.push(e.target.value)
-    }
-  }
-
-  function updateBio(e){
-    let intermdiateBio = e.target.value
-    console.log(e.target.value)
-    setBio(intermdiateBio)
-    console.log(intermdiateBio + ' this is the intermediate vlaue for bio to be passed on or discarded')
-    // setTestProfile(testProfile.bio = intermdiateBio)
-  }
-
-
-
-
-  function updateObj(){
-    let myObj = {
-      username: testUsername,
-      bio: testBio,
-      skills: testSkills,
-    }
-    console.log(myObj)
-    setTestProfile(myObj)
-    console.log(testProfile.bio)
+  function updateProfile(e){
+    setTestProfile({
+      ...testProfile,
+       [e.target.id]: e.target.value
+    })
     
   }
 
-  function saveData(){
-    if(testProfile.bio != ''){
-      setJohn(johnProfile.bio = testProfile.bio)
-    }
-    else if(testProfile.skills != []){
-      setJohn(johnProfile.skills = testProfile.skills)
-    }
-    else if(testProfile.goals != ''){
-      setJohn(johnProfile.goals = testProfile.goals)
-    }
-    else if(testProfile.favorite_GA_snacks != ''){
-      setJohn(johnProfile.favorite_GA_snacks = testProfile.favorite_GA_snacks)
-    }
-  
+  function toggleModal(){
+    setShowModal(!showModal)
+    setTestProfile(johnProfile)
   }
+
+
+  // adds one extra space (filled with an empty string) so taht skills has one extra than it did before
+  function addSkill(e){
+    e.preventDefault()
+    setTestProfile({
+      ...testProfile,
+      skills: [...testProfile.skills, '']
+    })
+  }
+
+
+  // function removeSkill(e){
+  //   e.preventDefault()
+  //   let copyOfTestSkills = testProfile.skills
+  //   let finalArr = copyOfTestSkills.filter((skill,index)=>{
+  //     copyOfTestSkills.indexOf(skill) != e.target.removalid
+  //   })
+  //   // console.log(copyOfTestSkills.splice(e.target.dataset.removalid,1))
+  //   // console.log(finalArr)
+  // }
+  function removeSkill(e){
+    e.preventDefault()
+    
+    setTestProfile({
+      ...testProfile,
+      skills: testProfile.skills.filter(skill=>
+        parseInt(testProfile.skills.indexOf(skill)) ===  parseInt(e.target.dataset.removalid)
+      )
+    })
+  }
+
+  // function removeSkill(e) {
+  //   e.preventDefault();
+  
+  //   const removeIndex = parseInt(e.target.dataset.removalid);
+  //   const updatedSkills = testProfile.skills.filter((skill, index) => index !== removeIndex);
+  
+  //   setTestProfile({
+  //     ...testProfile,
+  //     skills: updatedSkills
+  //   });
+  // }
+
+  // function removeSkill(e) {
+  //   e.preventDefault();
+  
+  //   const removeIndex = parseInt(e.target.dataset.removalid);
+  //   const updatedSkills = testProfile.skills.filter((_, index) => index !== removeIndex);
+  
+  //   setTestProfile(prevProfile => ({
+  //     ...prevProfile,
+  //     skills: updatedSkills
+  //   }));
+  // }
+  // function removeSkill(e) {
+  //   e.preventDefault();
+  
+  //   const removeIndex = parseInt(e.target.dataset.removalid, 10);
+  //   const updatedSkills = testProfile.skills.filter((_, index) => index !== removeIndex);
+  
+  //   setTestProfile(prevProfile => ({
+  //     ...prevProfile,
+  //     skills: updatedSkills
+  //   }));
+  // }
+  // function removeSkill(e) {
+  //   e.preventDefault();
+  
+  //   const removeIndex = parseInt(e.target.dataset.removalid, 10);
+  //   const updatedSkills = testProfile.skills.filter((_, index) => index !== removeIndex);
+  
+  //   setTestProfile(prevProfile => ({
+  //     ...prevProfile,
+  //     skills: updatedSkills
+  //   }));
+  // }
+  
+  
+  
+
+  function updateSkills(e){
+    e.preventDefault()
+
+    let updatedArr = testProfile.skills.map((skill,i)=>{
+      if(i == e.target.id){
+        return e.target.value
+      }
+      else{
+        return skill
+      }
+    })
+    
+    setTestProfile({
+      ...testProfile,
+      skills: updatedArr
+    }) 
+
+  }
+
+ 
+
+
+
+  function updateObj(e){
+    e.preventDefault()
+    let myObj = testProfile
+  
+    console.log(myObj)
+    setJohn(myObj)
+    console.log(johnProfile.bio)
+    toggleModal()
+    
+  }
+
+
 
   return (
     <div>
-      <ReactModal  isOpen={pop}> 
+      <ReactModal  isOpen={showModal} ariaHideApp={false}> 
         {/*set our content in here*/}
-        <form id='editBio'>
+        <form id='editProfile' onSubmit={updateObj}>
           <h5>Bio:</h5>
-            <textarea id='bioContent' onChange={updateBio} defaultValue={johnProfile.bio}></textarea>
+            <input id='bio' onChange={updateProfile} defaultValue={testProfile.bio}></input>
           <h5>Skills:</h5>
-            {johnProfile.skills.map((result) => (
-              <input id={johnProfile.skills.indexOf(result)} onChange={updateSkills} className='editableSkill' type='text' defaultValue={result}></input>
+          <ul>
+            {testProfile.skills.map((result, i) => (
+              <li key={`${i}Display`}>
+              <input id={i} onChange={updateSkills} className='editableSkill' type='text' defaultValue={result}></input>
+              <button data-removalid={i} onClick={removeSkill}>- Remove Skill</button>
+              </li>
             ))}
-          
+          </ul>
+            <button key='addSkill' onClick={addSkill}>Add Skill +</button>
             {/* <textarea onChange={updateGoals} defaultValue = {johnProfile.goals}></textarea> */}
-            <button onClick={updateObj} form="editBio" type='submit' >Submit</button>
+            <br/>
+
+            
+
+            <input form="editProfile" type='submit' value={'Submit'} />
         </form>
-        <button  onClick={popUp}>cancel</button>
+        <button  onClick={toggleModal}>cancel</button>
       </ReactModal>
+
         <NavBar />
+
+
         <div className='left'>
-          <div id='profileImg'></div>
-          <div id='media'></div>
+          <div id='profileImg'><img></img></div>
+          <div id='media'> <p id='technology'></p> </div>
         </div>
        
         <div className='right'>
           <div id='info' style={{ borderColor:'solid black'}}>
 
-            <svg xmlns="http://www.w3.org/2000/svg" onClick={popUp}  width="30" height="10" style={{marginLeft: 'px'}} >
-              <ellipse cx="5" cy="5" rx="4" ry="4" fill="grey"/>
-              <ellipse cx="15" cy="5" rx="4" ry="4" fill="grey"/>
-              <ellipse cx="25" cy="5" rx="4" ry="4" fill="grey"/>
-            </svg>
+              <button id='editButton' onClick={toggleModal}>Edit</button>
+        
 
             <div id='userName' className='infoDiv'>
               <h3 className='content'>{johnProfile.username}</h3>
@@ -122,17 +213,10 @@ export default function Profile(props) {
               <h3>Skills:</h3> 
                 <ul id='skillsList'>
                  {johnProfile.skills.map((result) => (
-                  <li className='content'>{result}</li>
+                  <li id={`${johnProfile.skills.indexOf(result)}Display`} className='content'>{result}</li>
                  ))}
                 </ul>
             </div>
-           
-            {/* <div id='goalsDiv' className='infoDiv'>
-              <h3>Goals:</h3> 
-              <p id='goalText' className='content'>
-                {johnProfile.goals}
-              </p>
-            </div> */}
             
 
             
